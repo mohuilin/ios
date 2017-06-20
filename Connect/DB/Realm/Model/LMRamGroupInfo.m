9708fc9572ea46e695a04b6ed371d01e374a582d
ldg <diange.liu@bitmain.com>
2017-06-20 16:03:59 +0800

    add lmrangroupInfo model

---
+++ Connect/DB/Realm/Model/LMRamGroupInfo.m
@@ -0,0 +1,13 @@
+//
+//  LMRamGroupInfo.m
+//  Connect
+//
+//  Created by Connect on 2017/6/20.
+//  Copyright © 2017年 Connect. All rights reserved.
+//
+
+#import "LMRamGroupInfo.h"
+
+@implementation LMRamGroupInfo
+
+@end
