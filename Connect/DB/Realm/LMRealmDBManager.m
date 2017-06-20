fe1a80b9dc48a528644fbfd51a3b87ca62689b19
MoHuilin <56434735@qq.com>
2017-06-20 16:01:10 +0800

    add realm mic

--- Connect/DB/Realm/LMRealmDBManager.m
+++ Connect/DB/Realm/LMRealmDBManager.m
@@ -13,39 +13,25 @@
 + (void)saveRecentChat:(LMRecentChat *)recentChat{
     NSArray *docPath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *path = [docPath objectAtIndex:0];
-    NSString *filePath = [path stringByAppendingPathComponent:@"test.realm"];
+    NSString *fileName = [NSString stringWithFormat:@"%@.realm",[[LKUserCenter shareCenter] currentLoginUser].address];
+    NSString *filePath = [path stringByAppendingPathComponent:fileName];
     NSLog(@"数据库目录 = %@",filePath);
     
     RLMRealmConfiguration *config = [RLMRealmConfiguration defaultConfiguration];
     config.fileURL = [NSURL URLWithString:filePath];
     config.readOnly = NO;
-    int currentVersion = 2.0;
-    config.schemaVersion = currentVersion;
-    
+    config.schemaVersion = 1.0;
     config.migrationBlock = ^(RLMMigration *migration , uint64_t oldSchemaVersion) {
-        if (oldSchemaVersion < currentVersion) {
-            [migration enumerateObjects:LMRecentChat.className block:^(RLMObject *oldObject, RLMObject *newObject) {
-                if (currentVersion == 2) {
-                    newObject[@"identifier2"] = @"";
-                }
-            }];
-        }
+        
     };
     [RLMRealmConfiguration setDefaultConfiguration:config];
-    
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-    
         RLMRealm *realm = [RLMRealm defaultRealm];
         // Updating book with id = 1
         [realm beginWriteTransaction];
         [realm addOrUpdateObject:recentChat];
         [realm commitWriteTransaction];
     });
-    
-    RLMRealm *realm = [RLMRealm defaultRealm];
-    [realm beginWriteTransaction];
-    recentChat.unReadCount = 1234;
-    [realm commitWriteTransaction];
 }
 
 @end
--- Connect/DB/Realm/Model/LMRecentChat.h
+++ Connect/DB/Realm/Model/LMRecentChat.h
@@ -10,9 +10,18 @@
 
 @interface LMRecentChat : LMBaseModel
 
-@property (nonatomic ,copy) NSString *identifier2;
+@property(nonatomic, copy) NSString *headUrl;
+@property(nonatomic, copy) NSString *name;
+@property(nonatomic, copy) NSString *time;
+@property(nonatomic, copy) NSString *content;
 @property(nonatomic, copy) NSString *identifier;
+@property(nonatomic, copy) NSString *draft;
 @property(nonatomic, assign) BOOL isTopChat;
+@property(nonatomic, assign) BOOL stranger;
+@property(nonatomic, assign) BOOL notifyStatus;
+@property(nonatomic ,assign) BOOL groupNoteMyself;
 @property(nonatomic, assign) int unReadCount;
+@property(nonatomic, assign) int snapChatDeleteTime;
+@property(nonatomic, assign) int talkType;
 
 @end
