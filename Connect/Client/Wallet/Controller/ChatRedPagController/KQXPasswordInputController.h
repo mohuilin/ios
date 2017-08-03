236cb780560849626c4770ffd525a33dfad8b20b
MoHuilin <56434735@qq.com>
2017-08-03 09:49:06 +0800

    add animation on view

--- Connect/Client/Wallet/Controller/ChatRedPagController/KQXPasswordInputController.h
+++ Connect/Client/Wallet/Controller/ChatRedPagController/KQXPasswordInputController.h
@@ -14,52 +14,25 @@
 @protocol KQXPasswordInputControllerDelegate <NSObject>
 
 @optional
-- (void)passwordInputControllerDidDismissed;
 
 - (void)passwordInputControllerDidClosed;
 
 @end
 
-typedef NS_ENUM(NSInteger, KQXPasswordInputStyle) {
-    KQXPasswordInputStyleWithoutMoney = 0,
-    KQXPasswordInputStyleWithMoney = 1
+typedef NS_ENUM(NSInteger, KQXPasswordCategory) {
+    KQXPasswordCategoryVerify = 0,
+    KQXPasswordCategorySet = 1
 };
 
-typedef void (^passwordInputComplete)(NSString *psw);
+typedef void (^CompleteWithSelfBlock)(KQXPasswordInputController *inputPassVc,NSString *psw);
 
 @interface KQXPasswordInputController : UIViewController
 
 @property(nonatomic, weak) id <KQXPasswordInputControllerDelegate> delegate;
 
-@property(nonatomic, copy) passwordInputComplete fillCompleteBlock;
 
-/**
- *  @brief Password input box construction method.。
- *  @param style Password input box style (0. do not need to show the money 1. need to show the money)
- */
-- (instancetype)initWithPasswordInputStyle:(KQXPasswordInputStyle)style;
+- (instancetype)initWithPasswordCategory:(KQXPasswordCategory)category complete:(CompleteWithSelfBlock)complete;
 
-/**
- *  @brief Password input box constructor class method.
- *  @param style Password input box style (0. do not need to show the money 1. need to show the money)）
- */
-+ (instancetype)passwordInputViewWithStyle:(KQXPasswordInputStyle)style;
+- (void)verfilySuccess:(BOOL)success;
 
-/** 
- *  @brief Set the title, subtitle, money strin。
- *  @param title title
- *
- */
-- (void)setTitleString:(NSString *)title descriptionString:(NSString *)description moneyString:(NSString *)money;
-
-/** @brief Error prompt view.。
- *  @param tip tips
- */
-- (void)showErrorTipWithString:(NSString *)tip;
-
-/**
- *  @brief Introduce the password input controller。
- *  @param isClosed Is not click off。
- **/
-- (void)dismissWithClosed:(BOOL)isClosed;
 @end
--- Connect/Client/Wallet/Controller/ChatRedPagController/KQXPasswordInputController.m
+++ Connect/Client/Wallet/Controller/ChatRedPagController/KQXPasswordInputController.m
@@ -6,28 +6,24 @@
 //  Copyright © 2016年 Asahi Kuang. All rights reserved.
 //
 
-@interface KQXPasswordInputController () <CAAnimationDelegate> {
+@interface KQXPasswordInputController (){
     CAShapeLayer *shapeLayer;
 }
 @property(weak, nonatomic) IBOutlet UIButton *closeButton;
 @property(weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property(weak, nonatomic) IBOutlet UILabel *descriptionLabel;
-@property(weak, nonatomic) IBOutlet UILabel *moneyLabel;
 @property(weak, nonatomic) IBOutlet UITextField *inputTextField;
 @property(weak, nonatomic) IBOutlet UIView *symbolView;
 @property(weak, nonatomic) IBOutlet UIView *bodyView;
-@property(weak, nonatomic) IBOutlet UIView *paySuccessView;
 
-@property(assign, nonatomic) KQXPasswordInputStyle style;
+@property(assign, nonatomic) KQXPasswordCategory category;
 
 @property(strong, nonatomic) NSMutableArray *spotArray;
 @property(strong, nonatomic) NSMutableArray *circleArray;
 @property(weak, nonatomic) IBOutlet NSLayoutConstraint *bodyCenterToX;
-@property(weak, nonatomic) IBOutlet NSLayoutConstraint *resultViewToBodyRight;
 
-@property(strong, nonatomic) KQXPasswordInputTipView *tip;
-
-@property(nonatomic, assign) CGFloat margin;
+@property (nonatomic ,copy) NSString *fristPassword;
+@property (nonatomic, copy) CompleteWithSelfBlock completeWithSelfBlock;
 
 @property(weak, nonatomic) IBOutlet NSLayoutConstraint *bodyViewHeightConstraint;
 @end
@@ -56,37 +52,14 @@
     return _circleArray;
 }
 
-- (KQXPasswordInputTipView *)tip {
-    if (!_tip) {
-        _tip = [KQXPasswordInputTipView sharedTipViewWithFrame:CGRectMake(0.f, -64.f, [UIScreen mainScreen].bounds.size.width, 64.f)];
-    }
-    return _tip;
-}
-
-#pragma mark --
-
-#pragma mark - initial method
-
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        [NSException raise:@"请使用initWithPasswordInputStyle:构造方法" format:@"!!!"];
-    }
-    return self;
-}
-
-- (instancetype)initWithPasswordInputStyle:(KQXPasswordInputStyle)style {
-    self = [super init];
-    if (self) {
+- (instancetype)initWithPasswordCategory:(KQXPasswordCategory)category complete:(CompleteWithSelfBlock)complete{
+    if (self = [super init]) {
         [self setModalPresentationStyle:UIModalPresentationOverCurrentContext];
         [self setModalTransitionStyle:UIModalTransitionStyleCrossDissolve];
-        _style = style;
+        self.category = category;
+        self.completeWithSelfBlock = complete;
     }
     return self;
-}
-
-+ (instancetype)passwordInputViewWithStyle:(KQXPasswordInputStyle)style {
-    return [[self alloc] initWithPasswordInputStyle:style];
 }
 
 #pragma mark --
@@ -94,97 +67,48 @@
 #pragma mark - life cycle
 
 - (void)viewDidLoad {
-
+    [super viewDidLoad];
+    
     [self drawCircle];
     [self drawSpot];
     [self observerAdded];
 
-    if (_style == KQXPasswordInputStyleWithMoney) {
-        [self updateConstraints];
+    switch (self.category) {
+        case KQXPasswordCategorySet:
+        {
+            self.titleLabel.text = LMLocalizedString(@"Wallet Set the password", nil);
+            self.descriptionLabel.text = LMLocalizedString(@"Wallet Enter 4 Digits", nil);
+        }
+            break;
+            
+        case KQXPasswordCategoryVerify:
+        {
+            self.titleLabel.text = LMLocalizedString(@"Wallet Verify password", nil);
+            self.descriptionLabel.text = LMLocalizedString(@"Wallet Enter 4 Digits", nil);
+        }
+            break;
+            
+        default:
+            break;
     }
-    [super viewDidLoad];
-
-    self.titleLabel.text = LMLocalizedString(@"Set Payment Password", nil);
-    self.descriptionLabel.text = LMLocalizedString(@"Wallet Enter 4 Digits", nil);
-}
-
-- (void)paySuccess {
-    
-    // circle path
-    CGPoint center = CGPointMake(self.paySuccessView.frame.size.width / 2, self.paySuccessView.frame.size.height / 2);// circle
-    float radius = self.paySuccessView.frame.size.width / 2;// raius
-
-    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:center radius:radius startAngle:M_PI * 3 / 2 endAngle:M_PI * 7 / 2 clockwise:YES];// Clockwise
-    path.lineCapStyle = kCGLineCapRound; // Line corner
-    path.lineJoinStyle = kCGLineCapRound; // End point processing
-
-    // Check the path
-    UIBezierPath *linePath = [UIBezierPath bezierPath];
-    [linePath moveToPoint:CGPointMake(center.x - radius / 2, center.y)];
-    [linePath addLineToPoint:CGPointMake(center.x, center.y + radius / 2)];
-    [linePath addLineToPoint:CGPointMake(center.x + radius * 2 / 3, center.y - radius / 3)];
-    // Stitching two paths
-    [path appendPath:linePath];
-
-    shapeLayer = [CAShapeLayer layer];
-    shapeLayer.lineJoin = kCALineJoinRound;
-    shapeLayer.lineCap = kCALineCapRound;
-    shapeLayer.path = path.CGPath;
-    shapeLayer.strokeColor = [UIColor blueColor].CGColor;// Line color
-    shapeLayer.fillColor = [UIColor clearColor].CGColor;// add color
-    shapeLayer.lineWidth = 1.7;
-    shapeLayer.strokeStart = 0.0;
-    shapeLayer.strokeEnd = 0.0;
-    [self.paySuccessView.layer addSublayer:shapeLayer];
-}
-
-- (void)startCircleAnimation {
-    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
-    if (shapeLayer.strokeEnd == 1.0) {
-        [animation setFromValue:@1.0];
-        [animation setToValue:@0.0];
-    } else {
-        [animation setFromValue:@0.0];
-        [animation setToValue:@1.0];
-    }
-    [animation setDuration:4];
-    animation.removedOnCompletion = NO;
-    animation.delegate = self;
-    animation.fillMode = kCAFillModeForwards;
-    [shapeLayer addAnimation:animation forKey:@"Circle"];
-}
-
-
-// update constant
-- (void)changeToSuccess {
-    self.bodyCenterToX.constant = -DEVICE_SIZE.width;
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
 
     [self clearContents];
-
     [super viewWillDisappear:animated];
-}
-
-- (void)viewWillAppear:(BOOL)animated {
-
-    [super viewWillAppear:animated];
+    
 }
 
 - (void)viewDidAppear:(BOOL)animated {
 
     [_inputTextField becomeFirstResponder];
-
     [super viewDidAppear:animated];
+    
 }
 
 - (void)viewDidLayoutSubviews {
 
-    if (self.margin == 0) {
-        self.margin = self.bodyView.left;
-        self.resultViewToBodyRight.constant = self.margin * 2;
-    }
     CGFloat symbol_total = (k_BODY_VIEW_WIDTH - 100);
     CGFloat spot_halfWidth = K_SPOT_RADIUS / 2;
     CGFloat spot_x = symbol_total / K_NUMBER_PASSWORD;
@@ -205,55 +129,13 @@
     [[NSNotificationCenter defaultCenter] removeObserver:self name:UITextViewTextDidChangeNotification object:_inputTextField];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 #pragma mark - Methods
-
-- (void)dismissWithClosed:(BOOL)isClosed {
-
-    [_inputTextField resignFirstResponder];
-    [self dismissViewControllerAnimated:NO completion:^{
-        if (isClosed) {
-            if ([self.delegate respondsToSelector:@selector(passwordInputControllerDidClosed)]) {
-                [self.delegate passwordInputControllerDidClosed];
-            }
-            return;
-        }
-        if ([self.delegate respondsToSelector:@selector(passwordInputControllerDidDismissed)]) {
-            [self.delegate passwordInputControllerDidDismissed];
-        }
-    }];
-}
-
-- (void)setTitleString:(NSString *)title descriptionString:(NSString *)description moneyString:(NSString *)money {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [self.titleLabel setText:title];
-        [self.descriptionLabel setText:description];
-        [self.moneyLabel setText:money];
-
-        // refresh
-        [self.view layoutIfNeeded];
-    });
-}
-
-- (void)showErrorTipWithString:(NSString *)tip {
-    [self.tip showTipViewWithStyle:KQXPasswordInputTipViewStyleError tip:tip];
-}
 
 - (void)clearContents {
     [_inputTextField setText:nil];
     for (CAShapeLayer *spot in self.spotArray) {
         [spot setHidden:YES];
     }
-}
-
-- (void)updateConstraints {
-    [_moneyLabel setHidden:NO];
-    _bodyViewHeightConstraint.constant = 181;
-
 }
 
 - (void)observerAdded {
@@ -299,24 +181,71 @@
         CAShapeLayer *spot = (CAShapeLayer *) obj;
         spot.hidden = idx < length ? NO : YES;
     }];
-
     if (length == K_NUMBER_PASSWORD) {
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t) (0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [self clearContents];
         });
-        self.fillCompleteBlock(textField.text);
+        switch (self.category) {
+            case KQXPasswordCategorySet:
+            {
+                if (self.fristPassword) {
+                    if ([self.fristPassword isEqualToString:textField.text]) {
+                        [self dismiss:nil];
+                        if (self.completeWithSelfBlock) {
+                            self.completeWithSelfBlock(self,self.fristPassword);
+                        }
+                    } else { /// tip pass not match
+                        self.titleLabel.text = LMLocalizedString(@"Wallet Set the password", nil);
+                        self.descriptionLabel.text = LMLocalizedString(@"Wallet The two passwords are not consistent please input again", nil);
+                        [self shakeTipLabel];
+                    }
+                } else {
+                    self.titleLabel.text = LMLocalizedString(@"Wallet Set the password", nil);
+                    self.descriptionLabel.text = LMLocalizedString(@"Wallet Please enter the four digit Numbers again", nil);
+                    self.fristPassword = textField.text;
+                }
+            }
+                break;
+                
+            case KQXPasswordCategoryVerify:
+            {
+                if (self.completeWithSelfBlock) {
+                    self.completeWithSelfBlock(self,textField.text);
+                }
+            }
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+- (void)shakeTipLabel{
+    CAKeyframeAnimation *shakeAnim = [CAKeyframeAnimation animation];
+    shakeAnim.keyPath = @"transform.translation.x";
+    shakeAnim.duration = 0.15;
+    CGFloat delta = 10;
+    shakeAnim.values = @[@0 , @(-delta), @(delta), @0];
+    shakeAnim.repeatCount = 2;
+    [self.descriptionLabel.layer addAnimation:shakeAnim forKey:nil];
+}
+
+- (void)verfilySuccess:(BOOL)success{
+    if (success) {
+        [self dismiss:nil];
+    } else {
+        self.descriptionLabel.text = LMLocalizedString(@"Login Password incorrect", nil);
+        [self shakeTipLabel];
     }
 }
 
 - (IBAction)dismiss:(id)sender {
-    [self dismissWithClosed:YES];
-}
-
-#pragma mark -- CAAnimationDelegate
-
-- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
-    [self.paySuccessView.layer removeAllAnimations];
-    [self dismiss:nil];
+    [_inputTextField resignFirstResponder];
+    [self dismissViewControllerAnimated:NO completion:^{
+        if ([self.delegate respondsToSelector:@selector(passwordInputControllerDidClosed)]) {
+            [self.delegate passwordInputControllerDidClosed];
+        }
+    }];
 }
 
 @end
